dc325edd1fb73e08c65bc873f7327b0bcf3bc8b2
Joocheol <joocheol@yonsei.ac.kr>
2024-06-12 13:26:09 +0900

    commit

---
+++ my_test.py
@@ -0,0 +1,18 @@
+from pfhedge.instruments import BrownianStock
+from pfhedge.instruments import EuropeanOption
+from pfhedge.nn import Hedger
+from pfhedge.nn import MultiLayerPerceptron
+
+# Prepare a derivative to hedge
+derivative = EuropeanOption(BrownianStock(cost=1e-4))
+
+# Create your hedger
+model = MultiLayerPerceptron()
+hedger = Hedger(
+    model, inputs=["log_moneyness", "expiry_time", "volatility", "prev_hedge"]
+)
+
+# Fit and price
+hedger.fit(derivative)
+price = hedger.price(derivative)
+print(price)
